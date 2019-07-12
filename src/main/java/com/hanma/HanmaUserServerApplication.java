7096449067efa3be417263aaa1d47d8fbf4c5268
zym <2603814199@qq.com>
2019-07-12 20:48:41 +0800

    第一次上传

---
+++ src/main/java/com/hanma/HanmaUserServerApplication.java
@@ -0,0 +1,13 @@
+package com.hanma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanmaUserServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanmaUserServerApplication.class, args);
+	}
+
+}
